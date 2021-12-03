60dfbcbb782538ce0b7d24249a41b2df74c3a4ca
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2021-12-03 17:42:44 +0200

    Update news.js

--- src/routes/news.js
+++ src/routes/news.js
@@ -8,7 +8,7 @@ APIKEY = process.env.NEWS_API_KEY;
 Router.get("", async (req, res) => {
   try {
     const newsAPI = await axios.get(
-      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${APIKEY}`
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey=9c08be690efe4d41873c1d3bfae2ff47`
     );
     const data = newsAPI.data.articles;
     // console.log(data);
@@ -31,7 +31,7 @@ Router.post("/", async (req, res) => {
 
   try {
     const newsAPI = await axios.get(
-      `https://newsapi.org/v2/everything?from=2021-09-02&sortBy=popularity&apiKey=${APIKEY}&q=${search}`
+      `https://newsapi.org/v2/everything?from=2021-09-02&sortBy=popularity&apiKey=9c08be690efe4d41873c1d3bfae2ff47&q=${search}`
     );
     const data = newsAPI.data.articles;
     // console.log(search);
